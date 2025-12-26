f3bf04b610bd440678d2acb500a9f1f4160885b8
Ryan Campbell <ryan.campbell@gmail.com>
2025-12-26 16:07:51 -0600

    chore: Remove test changeset file

--- liquibase/changelogs/test-changeset.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset e2e-test:1
--- This changeset should trigger SqlSelectStarWarn check
-SELECT * FROM users;
-
---changeset e2e-test:2
--- This changeset should trigger ChangeDropTableWarn check  
-DROP TABLE old_data;
